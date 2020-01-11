065c76a1533c71a406160537181e41d6eb31fefd
Jacob McAuley <mcauleyj@uci.edu>
2020-01-11 10:29:11 -0800

    Updated CFL to contain PrivateRolls/Coinpiles/Folders for Rolls

--- src/criticalLoot.js
+++ src/criticalLoot.js
@@ -1,71 +1,34 @@
-import {CRITICAL_FUMBLE_LOOT} from "../config/config.js"
-import {CRITICAL_FUMBLE_CONFIG} from "../config/config.js"
 
+import {CriticalFumbleBase} from "./criticalFumbleBase.js"
 
-class CriticalLoot{
+class CriticalLoot extends CriticalFumbleBase{
     constructor(){
+        super();
         this.folderName = "Critical Fumble Loot";
+        this.color = "red";
         this.compendiumName = "criticalfumbleloot";
         this.moduleScope = "critical-fumble";
-        this.folderID;
-        this.tables = [];
+        this.privateRollsSettingName = "privateLootRolls";
         this.init();
     }
 
     init(){
-        Hooks.on("updateToken", (token, id, tokenData) => {
-            this.validateData(token.actor.id, tokenData);
-        })
-        this.ensureTableImport();
+        super.init();
+        Hooks.on("updateToken", this._onTokenUpdate.bind(this))
     }
 
-    async ensureTableImport(){
-        this.folderID = await this.handleFolder(); 
-        this.ensureTables();
-    }
-
-    async handleFolder(){
-        let folder = Folder.collection.entities.find(folder => {return folder.data.name === this.folderName});
-        if(!folder)
-            return (await Folder.create({name : this.folderName, type: "RollTable", color:"red", parent:null})).id;
-        else   
-            return folder.id;
-    }
-
-    async ensureTables(){
-        let criticalCompendium = game.packs.find(compendium => compendium.metadata.name === this.compendiumName);
-        await criticalCompendium.getIndex();
-        criticalCompendium.index.forEach(async tableEntry => {
-            let resultingTable = RollTable.collection.entities.find(function(table){ return table.name == tableEntry.name})
-            if(!resultingTable)
-                resultingTable = await this._generateTable(criticalCompendium, tableEntry.id);
-            this.tables.push(resultingTable);
-        });
-    }
-
-    async _generateTable(pack, entryID){
-        let tableData = (await pack.getEntity(entryID)).data;
-        tableData.folder = this.folderID;
-        await RollTable.create(
-            tableData, 
-            {
-                displaySheet: false
-            }
-        );
-    }
-   
-    validateData(actorId, quickData){
+    _onTokenUpdate(sceneData, sceneID, tokenData){
+        let actor = game.actors.tokens[tokenData._id];
         if(!game.settings.get(this.moduleScope, 'lootTables'))
             return
-        const actor = Actor.collection.get(actorId);
-        if(actor.isPC)
+        if(!actor || actor.isPC)
             return;
         if(!this.isValidTarget(actor))
             return
-        if(!this.isZeroHealth(quickData))
+        if(!this.isTokenKilled(tokenData))
             return
         let table = this.getTableByCR(actor)
-        if(table != undefined && game.user.isGM)
+        if(table && game.user.isGM)
             this.rollTable(table);
     }
 
@@ -81,30 +44,30 @@ class CriticalLoot{
     }
 
     getTableByCR(actor){
-        let cr = actor.data.data.details.cr;
+        let cr = getProperty(actor, "data.data.details.cr");
         if(cr >= 0 && cr <= 4)
-            return this.tables.find(tableName => tableName.data.name === `Critical-Fumble CR_0_4`);
+            return this.tables.find(table => table.name === `Critical-Fumble CR_0_4`);
         else if(cr >= 5 && cr <= 10)
-            return this.tables.find(tableName => tableName.data.name === `Critical-Fumble CR_5_10`);
+            return this.tables.find(table => table.name === `Critical-Fumble CR_5_10`);
         else if(cr >= 11 && cr <= 16)
-            return this.tables.find(tableName => tableName.data.name === `Critical-Fumble CR_0_4`);
+            return this.tables.find(table => table.name === `Critical-Fumble CR_0_4`);
         else if(cr >= 17)
-            return this.tables.find(tableName => tableName.data.name === `Critical-Fumble CR_17`);
-        else   
-            throw("Unexpected CR");
+            return this.tables.find(table => table.name === `Critical-Fumble CR_17`);
+        throw "Critical Fumble : Unexpected CR Value" // Helps diagnose possible errors.
     }
 
-    isZeroHealth(actor){
-        return actor.actorData.data.attributes.hp.value <= 0;
+    isTokenKilled(tokenData){
+        let hp = getProperty(tokenData, "actorData.data.attributes.hp.value");
+        return hp !== undefined && hp <= 0;
     }
 
     async rollTable(table){
         let [roll, result] = table.roll()
+        if(!result)
+            throw "Critical Fumble : Undefined result error" // Helps diagnose possible errors.
         result.img = "modules/critical-fumble/icons/coins.svg";
-        if (result !== null ) {
-            await table.draw({roll, result});
-        }   
-        let parsedRolls = result.flags['critical_fumble_actions'];
+        await this._customDraw({roll, result}, table);
+        let parsedRolls = getProperty(result.flags, "critical_fumble_actions") || []; 
         this.handleRolls(parsedRolls); 
     }
 
@@ -112,16 +75,18 @@ class CriticalLoot{
         let coins = []
         
         for(let i = 0; i < rolls.length; ++i){
-            let roll = this._roll(rolls[i].roll, rolls[i].description, rolls[i].multiplier); 
-            coins.push({coinAmount: roll, cointType: rolls[i].description});
+            let total = this._roll(rolls[i].roll, rolls[i].description, rolls[i].multiplier); 
+            coins.push({coinAmount: total, coinType: rolls[i].description});
         }
         if(game.settings.get('critical-fumble', 'coinDistribution'))
             this._coinDistributePrompt(coins)
     }
 
-    _roll = (newRoll, coin, multiplier) => {
+    _roll(newRoll, coin, multiplier){
         let roll = new Roll(`${newRoll} * ${multiplier}`).roll();
         let rollMode = "roll";
+        if(this.privateRollSetting)
+            rollMode = "gmroll";
         let speaker = "Critical Fumble";
         let flavor = `Rolls for ${coin}`
         let sound = undefined;
@@ -164,39 +129,57 @@ class CriticalLoot{
         let string = "";
         for(let i = 0; i < coins.length; ++i){
             if(i == coins.length - 1){
-                string += `${coins[i].coinAmount}${coins[i].cointType}.`
+                string += `${coins[i].coinAmount}${coins[i].coinType}.`
             }
             else{
-                string += `${coins[i].coinAmount}${coins[i].cointType} +`
+                string += `${coins[i].coinAmount}${coins[i].coinType} + `
             }
-            
         }
         return string;
     }
 
-    _coinDistribute(coins){
+    async _coinDistribute(coins){
         let users = game.users.entities;
-        var length = game.users.entities.length - 1;
+        let totalPlayers = game.users.entities.reduce((acc, user) => {
+            if(!user.isGM && user.character)
+                acc += 1; 
+            return acc;
+        }, 0)
+
         for(let i = 0; i < coins.length; ++i){
-            users.forEach(async user => {
-                if(user.character != undefined && user.isGM == false){
-                    let currency = duplicate(user.character.data.data.currency);
-                    currency[coins[i].cointType.toLowerCase()] += Math.ceil(parseInt(coins[i].coinAmount / length));
-                    await user.character.update({"data.currency" : currency});
+            let coinsToDistribute = this._coinCalculator(coins[i].coinAmount, totalPlayers, coins[i].coinType); 
+            for(let j = 0; j < users.length; ++j){
+                if(users[j].character != undefined && users[j].isGM == false){
+                    let currency = duplicate(users[j].character.data.data.currency);
+                    currency[coins[i].coinType.toLowerCase()] += coinsToDistribute;
+                    await users[j].character.update({"data.currency" : currency});
                 }
-            })
+            }
         }
-        return;
+    }
+
+    _coinCalculator(coinAmount, totalPlayers, coinType){
+        let coinRemainderKey = `coinPile${coinType}`;
+        let coinPile = game.settings.get(this.moduleScope, coinRemainderKey) || 0;
+        let coinDistrubution = Math.floor(coinAmount / totalPlayers);
+        coinPile += coinAmount % totalPlayers;
+        if(coinPile != 0 && coinPile >= totalPlayers){
+            let coinsAddedPerPlayer = Math.floor(coinPile / totalPlayers);
+            coinDistrubution += coinsAddedPerPlayer
+            game.settings.set(this.moduleScope, coinRemainderKey, coinPile - (coinsAddedPerPlayer * totalPlayers))
+        }
+        else
+            game.settings.set(this.moduleScope, coinRemainderKey, coinPile)
+        return coinDistrubution;
+    }
+    
+    get privateRollSetting(){
+        return game.settings.get(this.moduleScope, this.privateRollsSettingName)
     }
 
 }
 
-Hooks.on('init', () => {
-    CRITICAL_FUMBLE_CONFIG.forEach(function(setting){
-        game.settings.register(setting.module, setting.key, setting.settings);
-    });
-})
-
 Hooks.on("ready", () => {
-    CriticalLoot = new CriticalLoot();
+    if(game.user.isGM)
+        CriticalLoot = new CriticalLoot();
 })
